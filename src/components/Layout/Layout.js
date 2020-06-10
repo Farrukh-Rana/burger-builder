1c646546aaf8d5c45b66e9ba1c8006cbf7c116e3
Farrukh-Rana <farrukhrana765@gmail.com>
2020-06-10 02:57:54 +0200

    Created basic structure of the App

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const layout = props => {
+    return (
+        <React.Fragment>
+            <div>
+                Toolbar, Sidedrawer, Backdrop
+            </div>
+            <main>
+                {props.children}
+            </main>
+        </React.Fragment>
+    );
+}
+
+export default layout;
